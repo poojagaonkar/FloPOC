ca12873bb8a1424bdac1972089d94eef35d6bcd0
PoojaGaonkar <pooja.gaonkar@zevenseas.com>
2016-06-01 15:17:15 +0530

    Android:Added Navigation drawer

---
+++ POCFlorence/POCFlorence.Droid/Fragments/ContentFragment.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace POCFlorence.Droid.Fragments
+{
+    public class ContentFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            
+            View view = inflater.Inflate(Resource.Layout.contentlayout, container, false);
+            return view;
+        }
+    }
+}
